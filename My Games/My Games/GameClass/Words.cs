7dfbc102eccb510905407c8d79926832660cf4ec
Grzegorz Galas <grzegorzgalas10@gmail.com>
2022-07-08 15:26:18 +0200

    Add wisielec game

---
+++ My Games/My Games/GameClass/Words.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Games.GameClass
+{
+    internal class Words
+    {
+        List<string> RandomWords = new List<string>()
+        {
+            
+        };
+        List<string> FoodWords = new List<string>()
+        {
+
+        };
+        List<string> CountryWords = new List<string>()
+        {
+
+        };
+        List<string> CityWords = new List<string>()
+        {
+
+        };
+        List<string> SportWords = new List<string>()
+        {
+
+        };
+        List<string> FlowerWords = new List<string>()
+        {
+
+        };
+        List<string> AnimalWords = new List<string>();
+    }
+}
---
+++ My Games/My Games/Games/Wisielec.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace My_Games.Games
+{
+    partial class Wisielec
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.CategoryBox = new System.Windows.Forms.ComboBox();
+            this.NewGameBtn = new System.Windows.Forms.Button();
+            this.CategoryLbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Algerian", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(315, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Wisielec";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 47);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 315);
+            this.panel1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 368);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(28, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // CategoryBox
+            // 
+            this.CategoryBox.FormattingEnabled = true;
+            this.CategoryBox.Location = new System.Drawing.Point(188, 9);
+            this.CategoryBox.Name = "CategoryBox";
+            this.CategoryBox.Size = new System.Drawing.Size(121, 23);
+            this.CategoryBox.TabIndex = 3;
+            // 
+            // NewGameBtn
+            // 
+            this.NewGameBtn.Location = new System.Drawing.Point(444, 9);
+            this.NewGameBtn.Name = "NewGameBtn";
+            this.NewGameBtn.Size = new System.Drawing.Size(75, 23);
+            this.NewGameBtn.TabIndex = 4;
+            this.NewGameBtn.Text = "Nowa gra";
+            this.NewGameBtn.UseVisualStyleBackColor = true;
+            // 
+            // CategoryLbl
+            // 
+            this.CategoryLbl.AutoSize = true;
+            this.CategoryLbl.Location = new System.Drawing.Point(122, 13);
+            this.CategoryLbl.Name = "CategoryLbl";
+            this.CategoryLbl.Size = new System.Drawing.Size(60, 15);
+            this.CategoryLbl.TabIndex = 5;
+            this.CategoryLbl.Text = "Kategorie:";
+            // 
+            // Wisielec
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 404);
+            this.Controls.Add(this.CategoryLbl);
+            this.Controls.Add(this.NewGameBtn);
+            this.Controls.Add(this.CategoryBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Wisielec";
+            this.Text = "Wisielec";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private TextBox textBox1;
+        private ComboBox CategoryBox;
+        private Button NewGameBtn;
+        private Label CategoryLbl;
+    }
+}
