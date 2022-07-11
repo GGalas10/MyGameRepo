217453733f83906509bd2b7d942df268b8dfb1c2
Grzegorz Galas <grzegorzgalas10@gmail.com>
2022-07-11 10:56:28 +0200

    Add words to list.

--- My Games/My Games/GameClass/Words.cs
+++ My Games/My Games/GameClass/Words.cs
@@ -10,19 +10,29 @@ namespace My_Games.GameClass
     {
         List<string> RandomWords = new List<string>()
         {
-            
+            "zremisować","zniżkować","zluzować","zdyspalatalizować","zatamować",
+            "zakochiwać","wyżywać","wyłamywać","wykukiwać","wmaszerowywać","werblować",
+            "sfalować","rozdenerwować","poujmować","gospodarować","żyźny","świadomościowy",
+            "łakny","zwierzchny","zamieszkalny","wąskotorowy","toksyczny","stratygraficzny","stracony"
         };
         List<string> FoodWords = new List<string>()
         {
-
+            "Barszcz","Kasza","Kluski","Kaszanka","Kapuśniak","Kotlet","Mielone","Makowiec","Mazurki","Herbatniki",
+            "Pierogi","Pizza","Flaczki","Naleśniki","Rolada","Racuchy","Piernik","Pączek","Grochówka","Faworki",
+            "Spaghetti","Stek","Gulasz","Golonka"
         };
         List<string> CountryWords = new List<string>()
         {
-
+            "Andora","Włochy","Polska","Francja","Niemcy","Czechy","Węgry","Szwecja","Ukraina","Hiszpania",
+            "Holandia","Norwegia","Dania","Austria","Słowacja","Malta","Serbia","Estonia","Belgia","Grecja",
+            "Irlandia","Wielka Brytania","USA","Albania","Bułgaria","Chorwacja","Portugalia","Litwa","Łotwa"
         };
         List<string> CityWords = new List<string>()
         {
-
+          "Warszawa"," Kraków"," Łódź"," Wrocław"," Poznań"," Gdańsk"," Szczecin"," Bydgoszcz"," Lublin"," Białystok",
+          "Kluczbork","Lądek-zdrój","Lubin","Marki","Mielno","Ujście","Szczecinek","Olsztyn","Piaseczno","Przemyśl",
+          "Sandomierz","Srargard","Koszalin","Gdynia","Hel","Łeba","Katowice","Toruń","Wadowice","Żagań","Swarzędz",
+          "Suwałki","Sopot","Andrychów","Bobolice","Brzesko","Barwice","Chojnice","Dęblin"
         };
         List<string> SportWords = new List<string>()
         {
