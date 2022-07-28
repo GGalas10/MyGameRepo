8a7ba3f6d2c29a488060b88b49a44d05a555f96a
Grzegorz Galas <grzegorzgalas10@gmail.com>
2022-07-28 16:03:41 +0200

    MemTraining add logic.

--- My Games/My Games/Games/MemTraining.Designer.cs
+++ My Games/My Games/Games/MemTraining.Designer.cs
@@ -37,6 +37,8 @@
             this.TitleLbl = new System.Windows.Forms.Label();
             this.PicImage = new System.Windows.Forms.ImageList(this.components);
             this.BlackPic = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -123,11 +125,20 @@
             this.BlackPic.TransparentColor = System.Drawing.Color.Transparent;
             this.BlackPic.Images.SetKeyName(0, "Black.png");
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(367, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
             // MemTraining
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(459, 371);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.TitleLbl);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.PktLbl);
@@ -137,6 +148,7 @@
             this.Name = "MemTraining";
             this.Text = "MemTrain";
             this.Load += new System.EventHandler(this.MTLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -151,5 +163,6 @@
         private Label TitleLbl;
         private ImageList PicImage;
         private ImageList BlackPic;
+        private PictureBox pictureBox1;
     }
 }
--- My Games/My Games/Games/MemTraining.cs
+++ My Games/My Games/Games/MemTraining.cs
@@ -12,52 +12,43 @@ namespace My_Games.Games
 {
     public partial class MemTraining : Form
     {
-        private int _Pkt = 0;
+        #region Variable
+        private int _Pkt = 0,_ImageNum;
         List<PictureBox> pcBoxList = new List<PictureBox>();
+        Task tr1;
+        #endregion
         public MemTraining()
         {
             InitializeComponent();
         }
-
-        private void comboBox1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            e.Handled = true;
-        }
-        private void MTLoad(object sender, EventArgs e)
-        {
-            PktLbl.Text = "Punktacja: "+_Pkt;
-        }
-
-        private void StartBtn_Click(object sender, EventArgs e)
-        {
-            int HowManyImage;
-            if(!int.TryParse(comboBox1.Text,out HowManyImage))
-            {
-                MessageBox.Show("Wybrano błędną lczbę");
-                return;
-            }
-            CreateBox(HowManyImage - 1);
-        }
-
-
+        
         #region Methods
-        public void CreateBox(int How)
+        public void CreateBox()
         {
-            pcBoxList.Clear();
+            if (pcBoxList.Count > 0)
+            {
+                foreach (PictureBox pb in pcBoxList)
+                {
+                    panel1.Controls.Remove(pb);
+                }
+            }
+            pcBoxList.Clear();           
             panel1.Size = new Size(48,48);
             int x = 5, y = 5;
-            if (How < 5)
+            if (_ImageNum <= 5)
             {
-                panel1.Size = new Size(panel1.Size.Width + (How * 53), panel1.Size.Height);
-                for (int i = 0; i < How*2; i++)
+                panel1.Size = new Size(panel1.Size.Width + (_ImageNum * 53), panel1.Size.Height);
+                for (int i = 0; i <= (_ImageNum * 2)-1; i++)
                 {
                     PictureBox pcBox = new PictureBox();
                     pcBox.Location = new Point(x, y);
                     pcBox.Size = new Size(48, 48);
-                    pcBox.Image = BlackPic.Images[0];
+                    pcBox.Tag = "";
+                    pcBox.BackColor = Color.Black;
+                    pcBox.Click += new System.EventHandler(this.BoxClick);
                     pcBoxList.Add(pcBox);
                     x += 53;                   
-                    if (i == How)
+                    if (i == _ImageNum - 1)
                     {
                         panel1.Size = new Size(panel1.Size.Width, panel1.Size.Height+53);
                         y += 53;
@@ -68,15 +59,17 @@ namespace My_Games.Games
             else
             {
                 panel1.Size = new Size(panel1.Size.Width + (6 * 53), panel1.Size.Height);
-                for (int i = 0; i < How * 2; i++)
+                for (int i = 0; i <= (_ImageNum * 2)-1; i++)
                 {
                     PictureBox pcBox = new PictureBox();
                     pcBox.Location = new Point(x, y);
                     pcBox.Size = new Size(48, 48);
-                    pcBox.Image = BlackPic.Images[0];
+                    pcBox.Tag = "";
+                    pcBox.BackColor = Color.Black;
+                    pcBox.Click+= new System.EventHandler(this.BoxClick);
                     pcBoxList.Add(pcBox);
                     x += 53;                    
-                    if (i % 6==0)
+                    if (x>=313)
                     {
                         panel1.Size = new Size(panel1.Size.Width, panel1.Size.Height+53);
                         y += 53;
@@ -96,6 +89,100 @@ namespace My_Games.Games
                 this.Size = new Size(panel1.Size.Width + 40, Height);
             if (panel1.Size.Height >= this.Size.Height && panel1.Size.Height >277)
                 this.Size = new Size(Width, panel1.Size.Height+133);
+        }
+        public void AddImage()
+        {
+            Random rand = new Random();
+            List<int> ImageNumber = new List<int>();
+            for(int i=0;i< _ImageNum; i++)
+            {
+                ImageNumber.Add(i);
+                ImageNumber.Add(i);
+            }
+            foreach(var box in pcBoxList) { 
+                int temp = ImageNumber[rand.Next(0, ImageNumber.Count - 1)];
+                box.Name = temp.ToString();
+                ImageNumber.Remove(temp);
+
+            }
+        }
+        public void ImageCheck()
+        {
+            if (pcBoxList.Where(p => p.Tag.ToString() == "click").Count() < 2)
+                return;
+            List<PictureBox> temp = new List<PictureBox>();
+            temp.AddRange(pcBoxList.Where(p => p.Tag.ToString() == "click"));
+            if (temp[0].Name == temp[1].Name)
+            {
+                _Pkt++;
+                temp[0].Enabled = false;
+                temp[1].Enabled = false;
+                temp[0].Click -= BoxClick;
+                temp[1].Click -= BoxClick;
+                temp[0].Tag = "";
+                temp[1].Tag = "";
+            }
+            else
+            {
+                Thread.Sleep(1000);
+                temp[0].BackColor = Color.Black;
+                temp[1].BackColor = Color.Black;
+                temp[0].Image = null;
+                temp[1].Image = null;
+                temp[0].Tag = "";
+                temp[1].Tag = "";
+            }
+            if (pcBoxList.Where(p => p.Enabled == false).Count() == _ImageNum * 2)
+            {
+                MessageBox.Show("Zgadłeś wszystkie brawo !!!\nKliknij ok później nowa gra.\nPunkty przechodzą na nową grę");
+            }
+            return;
+        }
+        public void PointsChanger()
+        {
+            PktLbl.Text = "Punktacja: " + _Pkt;
+        }
+        #endregion        
+        #region Events
+        private void comboBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = true;
+        }
+        private void MTLoad(object sender, EventArgs e)
+        {
+            PointsChanger();
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(comboBox1.Text, out _ImageNum))
+            {
+                MessageBox.Show("Wybrano błędną lczbę");
+                return;
+            }
+            CreateBox();
+            AddImage();
+        }
+        private async void BoxClick(object sender, EventArgs e)
+        {
+
+            tr1 = new Task(ImageCheck);
+            PictureBox send = (PictureBox)sender;
+            if (send.BackColor == Color.Black)
+            {
+                int Imageid = int.Parse(send.Name.Trim());
+                send.Image = PicImage.Images[Imageid];
+                send.Tag = "click";
+                send.BackColor = DefaultBackColor;
+                ImageCheck();
+                PointsChanger();
+            }
+            else
+            {
+                send.Image = null;
+                send.BackColor = Color.Black;
+                send.Tag = "";
+            }           
         }
         #endregion
     }
